23c761d04bb3e1447a263a8caffbb42a68b35854
lawnmower-74 <tonapricot@gmail.com>
2024-11-26 13:06:48 +0900

    設定調整

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 ```bash
 $ docker-compose run server go run db/migrate/migrate.go
 ```
+
+## yarn install
+```bash
+$ docker-compose run client yarn install
+```
--- server/router/router.go
+++ server/router/router.go
@@ -1,13 +1,22 @@
 package router
 
 import (
+	"os"
 	"todo_app/controller"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func NewRouter(tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,
+			echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
+		AllowCredentials: true,
+	}))
 	e.GET("/", tc.GetAllTasks)
 	e.POST("/", tc.CreateTask)
 	e.PUT("/:taskId", tc.UpdateTask)
